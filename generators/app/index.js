529aab5c5b4f6ab84a91207d59d65fe016cfeb60
xLogic <xloigc@qq.com>
2019-06-14 18:29:30 +0800

    feat(template): add vue-pc-template

--- generators/app/index.js
+++ generators/app/index.js
@@ -20,6 +20,7 @@ const DEFAULT_DIR = 'web-app';
 const GIT_BASE = 'https://github.com/';
 const TPL_REPOSITORY = [
   { name: 'vue-h5-template', value: 'blockfe/vue-h5-template' },
+  { name: 'vue-pc-template', value: 'blockfe/vue-pc-template' },
   { name: 'nuxt-pc-template', value: 'blockfe/nuxt-pc-template' }
 ];
 const DEFAULT_TPL = 'blockfe/vue-h5-template';
